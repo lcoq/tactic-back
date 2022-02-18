852f8b3a8b208fb6bea506f7dc2aa41ea87fb2d5
Louis Coquio <louis.coquio@gmail.com>
2022-02-18 20:05:48 +0100

    Add projects#show to fix entries by month which may require projects load

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'sessions' => 'sessions#show'
   resources :users, only: :index
   resources :sessions, only: :create
-  resources :projects, only: %i{ index create update destroy }
+  resources :projects, only: %i{ index show create update destroy }
   resources :clients, only: %i{ index create update destroy }
 
   get '/entries', to: 'entries#running', constraints: ->(r) { r.params['filter'] && r.params['filter']['running'] == '1' }
